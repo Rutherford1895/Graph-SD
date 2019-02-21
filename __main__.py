afc072820593936eb8d3a3b168a480c87e48002a
Rutherford1895 <rutherford1895@gmail.com>
2019-02-21 02:34:58 +0100

    1st commit

---
+++ __main__.py
@@ -0,0 +1,132 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+STOCK = 'stock'
+FLOW = 'flow'
+VARIABLE = 'variable'
+PARAMETER = 'parameter'
+
+
+def linear(x, a=1, b=0):
+    return a * float(x) + b
+
+
+def subtract(x, y):
+    return float(x) - float(y)
+
+
+def division(x, y):
+    return float(x) / float(y)
+
+
+LINEAR = linear
+SUBTRACT = subtract
+DIVISION = division
+
+
+class Structure(object):
+    def __init__(self):
+        self.sfd = nx.MultiDiGraph()
+        self.simulation_time = 20
+        self.dt = 0.25
+
+    def add_element(self, name, element_type, function=None, value=None):
+        # this 'function' is a list, containing the function it self and its parameters
+        self.sfd.add_node(name, element_type=element_type, function=function, value=value)
+
+    def add_causality(self, from_element, to_element, function=None):
+        self.sfd.add_edge(from_element, to_element, function)
+
+    def display_elements(self):
+        print('All elements in this SFD:')
+        print(self.sfd.nodes.data())
+
+    def display_element(self, name):
+        print('Attributes of element {}:'.format(name))
+        print(self.sfd.nodes[name])
+
+    def display_causalities(self):
+        print('All causalities in this SFD:')
+        print(self.sfd.edges)
+
+    def display_causality(self, from_element, to_element):
+        print('Causality from {} to {}:'.format(from_element, to_element))
+        print(self.sfd[from_element][to_element])
+
+    # !!!!!!!!!!!!!!!!!!!!!! #
+    def calculate(self, name):
+        if self.sfd.nodes[name]['value'] != None:
+            return self.sfd.nodes[name]['value']
+        else:  # it's not a constant value but a function
+            params = self.sfd.nodes[name]['function'][1:]
+            for j in range(len(params)):  # use recursion to find the values of params, then --
+                params[j] = self.calculate(params[j])  # replace the param's name with its value.
+
+            return self.sfd.nodes[name]['function'][0](*params)
+
+    def step(self):
+        flows_dt = dict()  # have a dictionary of flows and their values in this dt, to be added to stocks afterward.
+
+        # find all flows
+        for element in self.sfd.nodes:  # loop through all elements in this SFD,
+            if self.sfd.nodes[element]['element_type'] == FLOW:  # if this element is a FLOW --
+                flows_dt[element] = 0  # make a position for it in the dict of flows_dt, initializing it with 0
+
+        # calculate flows
+        for flow in flows_dt.keys():
+            flows_dt[flow] = self.dt * self.calculate(flow)
+        print('All flows dt:', flows_dt)
+
+        # calculating changes in stocks
+        # have a dictionary of affected stocks and their changes, for one flow could affect 2 stocks.
+        affected_stocks = dict()
+        for flow in flows_dt.keys():
+            successors = list(self.sfd.successors(flow))  # successors of a flow into a list
+            print('Successors of {}: '.format(flow), successors)
+            for successor in successors:
+                if self.sfd.nodes[successor]['element_type'] == STOCK:  # flow may also affect elements other than stock
+                    if successor not in affected_stocks.keys():
+                        affected_stocks[successor] = flows_dt[flow]
+                    else:
+                        affected_stocks[successor] += flows_dt[flow]
+
+        # updating stocks values
+        for stock in affected_stocks.keys():
+            self.sfd.nodes[stock]['value'] += affected_stocks[stock]
+
+
+structure0 = Structure()
+
+structure0.add_element('stock0', STOCK, value=100)
+structure0.add_element('flow0', FLOW, function=[DIVISION, 'gap0', 'at0'])
+structure0.add_causality('flow0', 'stock0')
+
+structure0.add_element('goal0', PARAMETER, value=20)
+structure0.add_element('gap0', VARIABLE, function=[SUBTRACT, 'goal0', 'stock0'])
+structure0.add_causality('stock0', 'gap0')
+structure0.add_causality('goal0', 'gap0')
+
+structure0.add_element('at0', PARAMETER, value=5)
+structure0.add_causality('gap0', 'flow0')
+structure0.add_causality('at0', 'flow0')
+
+
+structure0.display_elements()
+structure0.display_element('stock0')
+structure0.display_element('flow0')
+structure0.display_causalities()
+structure0.display_causality('flow0', 'stock0')
+
+# Run a simulation
+simulation_steps = int(structure0.simulation_time/structure0.dt)
+stock_behavior = list()
+for i in range(simulation_steps):
+    print('\nExecuting Step {} :\n'.format(i))
+    structure0.step()
+    # structure0.display_elements()
+    stock_behavior.append(structure0.sfd.nodes['stock0']['value'])
+
+print('\nStock Behavior:', stock_behavior)
+
+plt.plot(stock_behavior)
+plt.show()
